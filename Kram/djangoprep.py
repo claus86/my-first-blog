e0212a41d07b8e29956e447bb7bf8c09b5c3cb1b
claus86 <sas.claudia@gmail.com>
2021-08-06 17:54:34 +0200

    Changed the HTML for the site.

---
+++ Kram/djangoprep.py
@@ -0,0 +1,20 @@
+print('Hello, Django girls!')
+
+if 3 > 2:
+    print('It works!')
+
+if 5 > 2:
+    print('5 is indeed greater than 2')
+else:
+    print('5 is not greater than 2')
+
+
+
+
+name = 'Sonja'
+if name == 'Ola':
+    print('Hey Ola!')
+elif name == 'Sonja':
+    print('Hey Sonja!')
+else:
+    print('Hey anonymous!')
